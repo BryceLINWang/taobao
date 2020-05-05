fb7940a6e742467747893a7056754934445f6a1c
王博林 <Brycewlinw@163.com>
2020-05-05 23:33:38 +0800

    完善上传

--- taobao/taobao-upload/src/main/java/edu/xalead/controller/UploadController.java
+++ taobao/taobao-upload/src/main/java/edu/xalead/controller/UploadController.java
@@ -1,30 +1,42 @@
 package edu.xalead.controller;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.domain.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("upload")
 public class UploadController {
+    @Resource
+    private FastFileStorageClient storageClient;
+
+    @Resource
+    private ThumbImageConfig thumbImageConfig;
     @PostMapping("image")
     public ResponseEntity<String> image(MultipartFile file){
-        String url = "http://localhost:8080/upload/";
+        String url = "http://image.taobao.com/";
         String contentType=file.getContentType();
-        String fileName=file.getOriginalFilename();
-        String fon = UUID.randomUUID() + fileName;
-        File fe=new File("D:\\jjj\\apache-tomcat-9\\apache-tomcat-9.0.12\\webapps\\upload"+fon);
+        String fullPath = null;
         try {
-            file.transferTo(fe);
+            InputStream inputStream = file.getInputStream();
+            StorePath storePath = storageClient.uploadFile(inputStream, file.getSize(), "jpg", null);
+            fullPath = storePath.getFullPath();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return ResponseEntity.ok(url+fon);
+
+        return ResponseEntity.ok(url + fullPath);
     }
 }
